5d461ea6693b25643b763324590f141d409c1afe
João Neto <desenvolvimento@jnweb.pt>
2026-08-30 13:45:57 +0100

    chore: remove the default inspire console command

--- routes/console.php
+++ routes/console.php
@@ -3,12 +3,6 @@
 declare(strict_types=1);
 
 use App\Console\Commands\PruneExpiredDocumentExports;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
 Schedule::command(PruneExpiredDocumentExports::class)->daily();
